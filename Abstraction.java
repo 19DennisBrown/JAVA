9e43f56744fab26eccb21440a0f84b36d625c084
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2024-12-05 09:28:15 +0300

    abstraction

--- Abstraction.java
+++ Abstraction.java
@@ -1,3 +1,32 @@
-public class Abstraction {
-    
+
+
+
+abstract class People{
+
+    public abstract String walkingStyle();
+
+    public void population(){
+
+        System.out.println("Everywhere on earth.");
+    }
+}
+
+class Men extends  People{
+    public String walkingStyle(){
+        return "Unique";
+    }
+
+}
+
+
+
+public class Abstraction{
+    public static void main(String[] args){
+
+        Men menObj = new Men();
+        menObj.walkingStyle();
+        menObj.population();
+
+        System.out.println("__End__");
+    }
 }
